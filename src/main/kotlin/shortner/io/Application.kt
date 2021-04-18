54824b26ade8d89b963c8903cd4dfb0a3f3d0048
Meghdut-Mandal <meghdut.windows@gmail.com>
2021-04-18 17:41:05 +0000

    CCompleted API

--- src/main/kotlin/shortner/io/Application.kt
+++ src/main/kotlin/shortner/io/Application.kt
@@ -4,6 +4,9 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import shortner.io.plugins.*
 
+
+
+
 fun main() {
     embeddedServer(Netty, port = 8086) {
         configureRouting()
--- src/main/kotlin/shortner/io/plugins/Routing.kt
+++ src/main/kotlin/shortner/io/plugins/Routing.kt
@@ -4,37 +4,62 @@ import io.ktor.routing.*
 import io.ktor.http.*
 import io.ktor.locations.*
 import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.gson.*
 import io.ktor.response.*
 import io.ktor.request.*
+import io.ktor.util.*
+import shortner.io.Datastore
+import java.util.*
 
 fun Application.configureRouting() {
     install(Locations) {
     }
+    install(ContentNegotiation) {
+        gson()
+    }
+
+    val rootUrl = Datastore["rootUrl"]
 
     routing {
-        get("/") {
-            call.respondText("Hello World!")
+        post<Generate> {
+            val urlLink = call.receive<LinkRequest>().link
+            val longCode = Base64.getEncoder().encodeToString(urlLink.toByteArray())
+            if (Datastore.allKeys.contains(longCode)) {
+                val shortCode =
+                    Datastore[longCode] ?: throw IllegalArgumentException("this should happen")
+                return@post call.respond(HttpStatusCode.OK, Data("$rootUrl/$shortCode"))
+            } else {
+                val shortCode = System.currentTimeMillis().toString(35)
+                Datastore[longCode] = shortCode
+                Datastore[shortCode] = longCode
+                return@post call.respond(HttpStatusCode.OK, Data("$rootUrl/$shortCode"))
+            }
+
         }
-        get<MyLocation> {
-            call.respondText("Location: name=${it.name}, arg1=${it.arg1}, arg2=${it.arg2}")
+
+        get<LoadUrl>{
+            if (Datastore.allKeys.contains(it.code)){
+                val longcode=Datastore[it.code]
+                val urlLink = String(Base64.getDecoder().decode(longcode))
+
+              return@get  call.respondRedirect(urlLink)
+            }
+          return@get  call.respond(HttpStatusCode.BadRequest,"Bad Url")
         }
-        // Register nested routes
-        get<Type.Edit> {
-            call.respondText("Inside $it")
-        }
-        get<Type.List> {
-            call.respondText("Inside $it")
-        }
+
     }
 }
 
-@Location("/location/{name}")
-class MyLocation(val name: String, val arg1: Int = 42, val arg2: String = "default")
-@Location("/type/{name}")
-data class Type(val name: String) {
-    @Location("/edit")
-    data class Edit(val type: Type)
+data class Data(val shortUrl: String)
+data class LinkRequest(val link: String)
 
-    @Location("/list/{page}")
-    data class List(val type: Type, val page: Int)
-}
+
+@Location("/generate")
+class Generate
+
+@Location("/{code}")
+data class LoadUrl(val code:String)
+
+
+
